f89823ef5b9ed79e777e2839fdfb726d5582f9ff
TOT102 <73033834+TOT102@users.noreply.github.com>
2024-03-14 17:17:17 +0200

    Redesigned the UI(UI V2)

--- app/src/main/java/com/example/sortingsystem/MainActivity.java
+++ app/src/main/java/com/example/sortingsystem/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
